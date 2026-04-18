e9fa56ab32e75ca1920def1eeeea48e3c5f2f769
audrey <audreym.sturdy@gmail.com>
2026-04-18 22:22:42 +0200

    feat(client): configure client routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './features/dashboard/pages/dashboard-page/dashboard-page';
+import { WorkflowsPage } from './features/workflows/pages/workflows-page/workflows-page';
+import { SubmissionsPage } from './features/submissions/pages/submissions-page/submissions-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardPage },
+  { path: 'workflows', component: WorkflowsPage },
+  { path: 'submissions', component: SubmissionsPage },
+  { path: '**', redirectTo: 'dashboard' }
+];
